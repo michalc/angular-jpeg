635aa64491feadaec9ccad81e951ff828ff4d6ca
Michal Charemza <michalcharemza@gmail.com>
2014-12-25 10:02:36 +0000

    Temporarily removing unused code

--- src/angular-jpeg.js
+++ src/angular-jpeg.js
@@ -1,34 +1,38 @@
+/*global angular */
+
 angular.module('angular-jpeg', []);
 
 angular.module('angular-jpeg').service('AngularJpeg', function($q, $window) {
-	
+	'use strict';
+
 	var ERRORS = {
 		'NO_FILE': 'No file found',
 		'UNKNOWN': 'Unknown error'
 	};
 
-	var MARKERS = {
-		startOfImage: 0xFFD8,
-		endOfImage: 0xFFD9
-	};
+	// var MARKERS = {
+	// 	startOfImage: 0xFFD8,
+	// 	endOfImage: 0xFFD9
+	// };
 
-	function view(buffer) {
-		return new $window.Int8Array(buffer);
-	}
+	// function view(buffer) {
+	// 	return new $window.Int8Array(buffer);
+	// }
 
-	function validateFile(int8View) {
-		new $window.Int8Array(buffer);
-	}
+	// function validateFile(buffer) {
+	// 	return new $window.Int8Array(buffer);
+	// }
 
 
 	function loadFromFile(file) {
-		if (!file) return $q.reject(ERRORS.NO_FILE);
+		if (!file) {
+			return $q.reject(ERRORS.NO_FILE);
+		}
 		var reader = new $window.FileReader();
 		var deferred = $q.defer();
 		
 		reader.onload = function(e) {
 			var buffer = e.target.result;
-			var view = view(buffer);
 			deferred.resolve(buffer);
 		};
 
