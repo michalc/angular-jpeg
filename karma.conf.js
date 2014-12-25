05b2cde0107ee7099a85a6494062cf2486cb28b9
Michal Charemza <michalcharemza@gmail.com>
2014-12-25 10:32:44 +0000

    Test framework

---
+++ karma.conf.js
@@ -0,0 +1,61 @@
+/*global module */
+
+module.exports = function(config) {
+  'use strict';
+  
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
+
+    // testing framework to use (jasmine/mocha/qunit/...)
+    frameworks: ['jasmine'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'src/*.js',
+      'test/*.js'
+    ],
+
+    // list of files / patterns to exclude
+    exclude: [],
+
+    // web server port
+    port: 8080,
+
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+    reporters: ['dots', 'coverage'],
+
+    preprocessors: {
+      'src/*.js': ['coverage']
+    },
+
+    coverageReporter: {
+      type: 'lcov',
+      dir: 'coverage/'
+    },
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: true
+  });
+};
---
+++ test/angular-jpeg-spec.js
@@ -0,0 +1,11 @@
+/*global module */
+
+describe('AngularJpeg', function () {
+  'use strict';
+
+  beforeEach(module('angular-jpeg'));
+
+  it('should expect one to be one', function() {
+     expect(1).toBe(1);
+  });
+});
