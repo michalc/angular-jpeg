4bddaac481424e77a71d756d4d4c0fbcb3625504
Michal Charemza <michalcharemza@gmail.com>
2015-01-11 20:56:27 +0000

    Test Worker

--- test/angular-jpeg-spec.js
+++ test/angular-jpeg-spec.js
@@ -1,4 +1,4 @@
-/*global angular, describe, module, beforeEach, it, expect, inject*/
+/*global angular, describe, module, beforeEach, it, expect, inject, jasmine*/
 
 describe('AngularJpeg', function () {
   'use strict';
@@ -58,6 +58,165 @@ describe('AngularJpeg', function () {
           args: [args]
         });
       });
+    });
+  });
+});
+
+describe('Worker', function () {
+  'use strict';
+  var Worker;
+  var ANGULAR_JPEG_CONFIG;
+  var workerInstance;
+  var WorkerMock;
+  var $rootScope;
+  beforeEach(module('angular-jpeg'));
+
+  beforeEach(module(function($provide) {
+    WorkerMock = function(args) {
+      this.args = args;
+      this.postMessage = jasmine.createSpy('postMessage');
+      workerInstance = this;
+    };
+    workerInstance = null;
+    $provide.value('$window', {
+      Worker: WorkerMock
+    });
+  }));
+
+  beforeEach(inject(function(_Worker_, _ANGULAR_JPEG_CONFIG_, _$rootScope_) {
+    Worker = _Worker_;
+    ANGULAR_JPEG_CONFIG = _ANGULAR_JPEG_CONFIG_;
+    $rootScope = _$rootScope_;
+  }));
+
+  it('creates a Worker instance', function() {
+    expect(workerInstance instanceof WorkerMock).toBe(true);
+  });
+
+  it('passes the correct path to the Worker instance', function() {
+    expect(workerInstance.args).toBe(ANGULAR_JPEG_CONFIG.ANGULAR_JPEG_PATH + '/angular-jpeg-worker.js');
+  });
+
+  it('passes config to the instance', function() {
+    expect(workerInstance.postMessage).toHaveBeenCalledWith(ANGULAR_JPEG_CONFIG);
+    workerInstance.postMessage.calls.reset();
+  });
+
+  describe('sending a command', function() {
+    var options = {
+      command: 'test-command',
+      args: ['arg1', 'arg2']
+    };
+
+    it('passes the options to postMessage with id of 1', function() {
+      var options = {
+        command: 'test-command',
+        args: ['arg1', 'arg2']
+      };
+
+      Worker(options);
+      expect(workerInstance.postMessage).toHaveBeenCalledWith({
+        id: 1,
+        command: options.command,
+        args: options.args
+      });
+    });
+
+    it('passes a second call with id of 2', function() {
+      Worker(options);
+      Worker(options);
+      expect(workerInstance.postMessage).toHaveBeenCalledWith(jasmine.objectContaining({
+        id: 2
+      }));
+    });
+  });
+
+  describe('resolving a command', function() {
+    it('done by posting a message with type "resolve"', function() {
+      var correctResults = {};
+      var returnedResults;
+      Worker({command: 'dummy'}).then(function(_returnedResults_) {
+        returnedResults = _returnedResults_;
+      });
+      workerInstance.onmessage({
+        data: {
+          id: 1,
+          type: 'resolve',
+          data: correctResults
+        }
+      });
+      $rootScope.$apply();
+      expect(returnedResults).toBe(correctResults);
+    });
+  });
+
+  describe('multiple in-progress commands', function() {
+    it('resolved correctly', function() {
+      var correctResults1 = {};
+      var correctResults2 = {};
+      var returnedResults1;
+      var returnedResults2;
+      Worker({command: 'dummy'}).then(function(_returnedResults1_) {
+        returnedResults1 = _returnedResults1_;
+      });
+      Worker({command: 'dummy'}).then(function(_returnedResults2_) {
+        returnedResults2 = _returnedResults2_;
+      });
+      workerInstance.onmessage({
+        data: {
+          id: 2,
+          type: 'resolve',
+          data: correctResults2
+        }
+      });
+      workerInstance.onmessage({
+        data: {
+          id: 1,
+          type: 'resolve',
+          data: correctResults1
+        }
+      });
+      $rootScope.$apply();
+      expect(returnedResults1).toBe(correctResults1);
+      expect(returnedResults2).toBe(correctResults2);
+    });
+  });
+
+  describe('rejecting a command', function() {
+    it('done by posting a message with type "reject"', function() {
+      var correctError = {};
+      var returnedError;
+      Worker({command: 'dummy'}).catch(function(_returnedError_) {
+        returnedError = _returnedError_;
+      });
+      workerInstance.onmessage({
+        data: {
+          id: 1,
+          type: 'reject',
+          data: correctError
+        }
+      });
+      $rootScope.$apply();
+      expect(returnedError).toBe(correctError);
+    });
+  });
+
+  describe('notifying a command', function() {
+    it('done by posting a message with type "notify"', function() {
+      var correctNotification = {};
+      var returnedNotification;
+      Worker({command: 'dummy'}).then(null, null, function(_returnedNotification_) {
+        returnedNotification = _returnedNotification_;
+      });
+      workerInstance.onmessage({
+        data: {
+          id: 1,
+          type: 'notify',
+          data: correctNotification
+        }
+      });
+      $rootScope.$apply();
+      expect(correctNotification).toBe(correctNotification);
     });
   });
 });
